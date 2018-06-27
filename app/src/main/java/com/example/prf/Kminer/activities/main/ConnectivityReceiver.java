9d3c0e3c41c3840449d62b3a103093c1e0931fe4
Pio Raffaele Fina <pioraffaelefina@outlook.it>
2018-06-27 21:17:00 +0200

    Add code to check internet connection and dispaly related snackbar messages

---
+++ app/src/main/java/com/example/prf/Kminer/activities/main/ConnectivityReceiver.java
@@ -0,0 +1,61 @@
+package com.example.prf.Kminer.activities.main;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.prf.Kminer.R;
+
+public class ConnectivityReceiver extends BroadcastReceiver {
+
+    private View rootView;
+
+    public ConnectivityReceiver(View rootView) {
+        this.rootView = rootView;
+    }
+
+    private static final String TAG = "ConnectivityReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle extras = intent.getExtras();
+
+
+
+        if (extras != null) {
+            ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo info = connectivityManager.getActiveNetworkInfo();
+            Log.d( TAG,  "info:" + info);
+
+
+            if( info != null && info.isConnectedOrConnecting()){
+                Snackbar.make(rootView,
+                        "Connection established",
+                        Snackbar.LENGTH_SHORT).show();
+
+            }
+            else {
+
+                final Snackbar snBar = Snackbar.make(rootView,
+                        R.string.error_noNetwork,
+                        Snackbar.LENGTH_INDEFINITE);
+                snBar.setAction("Dismiss", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        snBar.dismiss();
+                    }
+                });
+                snBar.show();
+            }
+
+
+        }
+    }
+}
--- app/src/main/java/com/example/prf/Kminer/activities/main/MainActivity.java
+++ app/src/main/java/com/example/prf/Kminer/activities/main/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.prf.Kminer.activities.main;
 
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -7,6 +9,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.prf.Kminer.R;
 
@@ -18,19 +21,22 @@ import java.util.List;
  * The logic is delegated to the relate Fragments
  */
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "ActivityMain";
     private ViewPager pager;
+    private ConnectivityReceiver connectivityReceiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Set ActionBar
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME |
                 ActionBar.DISPLAY_SHOW_TITLE | ActionBar.DISPLAY_HOME_AS_UP | ActionBar.DISPLAY_USE_LOGO);
         getSupportActionBar().setIcon(R.drawable.ic_home_black_24dp);
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
 
+        // Set viewPager and TabLayout
         TabPageAdapter adapter = new TabPageAdapter(getSupportFragmentManager(),
                 getFragments(),
                 new String[] {"Discover","Read"});
@@ -41,8 +47,31 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = findViewById(R.id.activity_main_tabs);
         tabLayout.setupWithViewPager(pager);
 
+        // Register receiver to listen network state changes
+        connectivityReceiver = new ConnectivityReceiver(findViewById(R.id.activity_main_rootLayout));
+        registerReceiver( connectivityReceiver,
+                new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+
+        Log.d(TAG,"onCreate() called");
+
+    }
 
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(connectivityReceiver);
+
+        Log.d(TAG,"onPause() called");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(connectivityReceiver != null){
+            registerReceiver( connectivityReceiver,
+                    new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+        }
     }
 
     /**
@@ -57,4 +86,6 @@ public class MainActivity extends AppCompatActivity {
 
         return fragmentsArray;
     }
+
+
 }
