c6e4b444db3395770fbaf4f1ecc4b7726245ec27
Pio Raffaele Fina <pioraffaelefina@outlook.it>
2018-07-03 23:41:42 +0200

    Add javadoc to ClustersActivity class

--- app/src/main/java/com/example/prf/Kminer/activities/clusters/ClustersActivity.java
+++ app/src/main/java/com/example/prf/Kminer/activities/clusters/ClustersActivity.java
@@ -10,30 +10,44 @@ import android.widget.TextView;
 import com.example.prf.Kminer.R;
 import com.example.prf.Kminer.models.ClusterSet;
 
+/**
+ * Activity for showing the cluster-set.
+ *
+ * This activity show a list of clusters using a RecyclerView.
+ * This activity must receive a {@link com.example.prf.Kminer.models.ClusterSet}
+ * instance in the incoming intent Bundle
+ */
 public class ClustersActivity extends AppCompatActivity {
 
     private static final String TAG = "ClustersActivity";
 
+    /**
+     * Framework method called on create lifecycle state to setup the UI.
+     *
+     * @param savedInstanceState previous saved bundle
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clusters);
 
         ClusterSet clusterSet = (ClusterSet) this.getIntent().getSerializableExtra("clusters");
-
         TextView discoveredTxt = findViewById(R.id.showclusters_txt_discovered);
-        discoveredTxt.setText(Html.fromHtml(getString(R.string.discovered_text, clusterSet.getExamplesNumber(), clusterSet.getSize())));
 
-        initRecyclerView(clusterSet);
-
+        if(clusterSet != null) {
+           discoveredTxt.setText(Html.fromHtml(getString(R.string.discovered_text, clusterSet.getExamplesNumber(), clusterSet.getSize())));
+           initRecyclerView(clusterSet);
+        } else {
+            discoveredTxt.setText("No cluster set available!");
+        }
 
         getSupportActionBar().setTitle(R.string.title_actionBar_clusters);
-
-
-
     }
 
-
+    /**
+     * Utility method to setup the RecyclerView and inject the clusterset into the RecyclerView adapter
+     * @param clusters
+     */
     private void initRecyclerView(ClusterSet clusters){
 
         ClustersRecyclerViewAdapter adapter = new ClustersRecyclerViewAdapter(clusters, this);
